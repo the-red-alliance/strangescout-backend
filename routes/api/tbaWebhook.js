509f6a6c8e0ef7fd62a6ff3a1f6d910672ebc222
Noah Piraino <penguinsnail@onebuttonmouse.com>
2020-01-25 11:00:06 -0500

    typo

--- routes/api/tbaWebhook.js
+++ routes/api/tbaWebhook.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 	console.log('Webhook POST: ', { rawHeaders: req.rawHeaders, body: req.body });
 	if (!req.header('X-TBA-HMAC') || !process.env.TBA_HMAC) return res.send(200);
-	if (req.header.header('X-TBA-HMAC') !== process.env.TBA_HMAC) return res.send(401);
+	if (req.header('X-TBA-HMAC') !== process.env.TBA_HMAC) return res.send(401);
 });
 
 module.exports = router;
