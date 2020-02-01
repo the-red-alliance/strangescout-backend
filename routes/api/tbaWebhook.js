a1250237e9134061269c8167a5d22ad9e985784d
Noah Piraino <penguinsnail@onebuttonmouse.com>
2020-02-01 11:13:45 -0500

    tba webhook triggers matches update

--- routes/api/tbaWebhook.js
+++ routes/api/tbaWebhook.js
@@ -1,6 +1,8 @@
 //const mongoose = require('mongoose');
 const router = require('express').Router();
 
+const eventsUtils = require('../../utils/events');
+
 router.get('/', (req, res) => {
 	console.log('Webhook GET: ', { headers: req.headers, body: req.body });
 	return res.send(200);
@@ -12,6 +14,14 @@ router.post('/', (req, res) => {
 	if (req.header('X-TBA-HMAC') !== process.env.TBA_HMAC) return res.send(401);
 
 	console.log('Hook authorized!');
+
+	switch (req.body.message_type) {
+	case 'schedule_updated':
+		eventsUtils.getMatches(req.body.message_data.event_key);
+		break;
+	default:
+		return;
+	}
 });
 
 module.exports = router;
