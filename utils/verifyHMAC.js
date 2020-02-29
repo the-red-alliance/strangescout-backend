e27da29bc9f91edc6bf2f9db350b640bc9fc103e
Noah Piraino <penguinsnail@onebuttonmouse.com>
2020-02-29 15:34:00 -0500

    try stringifying HMAC

--- utils/verifyHMAC.js
+++ utils/verifyHMAC.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto');
 
 module.exports = (secret, data, hmac) => {
-	const hash = crypto.createHmac('sha256', secret).update(data).digest('hex');
+	const hash = crypto.createHmac('sha256', secret).update(JSON.stringify(data)).digest('hex');
+	console.log('Given HMAC: ' + hmac);
+	console.log('Generated HMAC: ' + hash)
 	return (hash === hmac);
 }
